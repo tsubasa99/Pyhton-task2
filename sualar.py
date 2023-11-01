52e7aa9fe29bacf9ffe19ad1789cf2bf11d84074
tsubasa99 <tunar.mm99@gmail.com>
2023-11-01 23:54:17 +0400

    first

---
+++ sualar.py
@@ -0,0 +1,117 @@
+# 2 ci sual
+# Ruslan = input("Adınızı daxil edin: ")
+# print("Salam " + Ruslan + ", xoş gəldin!")
+
+# 3 cu sual
+
+# blue_while = 0
+# second_while = 1
+
+# while second_while <= 100:
+#     blue_while += second_while
+#     second_while += 1
+# print(blue_while)
+# blue_for = 0
+# for sayi in range(1, 101):
+#     blue_for += sayi
+# print(blue_for)
+
+# 4 cu sual 
+
+# 3 cu sualin cavabi 
+
+# dictionary key value olur
+# sirali olmur
+# {}
+
+# tuple degerini deyise bilmirik inmutable
+# ()
+# list degistirilebilir
+# []
+# deger eklene biler cixarila biler deyisdirile biler
+# set icerisinde bir deger bir defe istifade oluna biler
+# sira anlayisi vardir
+
+
+
+# # 6 ci sual 
+# list = ["nergiz", "tural", "cavad", "Ruslan", "orxan"]
+# for index in range(len(list)):
+#     royal = list[index]
+#     red = len(royal)
+#     print(f"Index {index}: '{royal}' {red}.")
+
+
+
+
+# 5 ci sual 
+# dict1 = {"key1": "value1", "key2": "value2"}
+# dict2 = {"key3": "value3", "key4": "value4"}
+# dict1.update(dict2)
+
+# print(dict1)
+
+# 7 ci sual 
+# my_tuple = (1, 2, 2, 3, 4, 2)
+# count = my_tuple.count(2)
+# print(count)
+
+
+# my_tuple = (1, 2, 3, 4, 5)
+# index = my_tuple.index(3)
+# print(index)
+
+# 9 cu sualin 1 cisi 
+# def reverse (deger):
+#     rev = str(deger)[::-1]
+#     return rev
+# pink = input("Bir değer girin: ")
+# rev = reverse(pink)
+# print("Ters çevrilmiş değer:",rev)
+
+
+# 1 ci sual 
+# number1 = float(input("Birinci ededi girin:  "))
+# number2 = float(input("İkinci ededi girin:  "))
+
+# toplama_sonucu = number1 + number2
+# print("Toplama: ", netice)
+
+
+# cikarma_sonucu = number1 - number2
+# print("Çıxma: ", netice)
+
+
+# carpma_sonucu = number1 * number2
+# print("Vurma: ", netice)
+
+# if number2 != 0:
+#     bolme_sonucu = number1 / number2
+#     print("Bölme: ", bolme_sonucu)
+# else:
+#     print("Sıfıra bolunmez.")
+
+
+# 8 ci sual 
+# ascii_characters = []
+# ascii_values = []
+# for i in range(5):
+#     ascii_characters.append(chr(i))  
+#     ascii_values.append(i)
+# print(ascii_characters)
+# print (ascii_values)
+
+# 1 ci sual
+# eded1 = float(input("Birinci sayıyı girin: "))
+# eded2 = float(input("İkinci sayıyı girin: "))
+# toplam = eded1 + eded2
+# print("Toplama cavbi: ", toplam)
+# cikarma = eded1 - eded2
+# print("Çıxma cavabi: ", cikarma)
+# carpma = eded1 * eded2
+# print("Vurma cavabi: ", carpma)
+# if eded2 != 0:
+#     bolme = eded1 / eded2
+#     print("Bölme cavabi: ", bolme)
+# else:
+#     print("Bir sayıyı 0'a bölemezsiniz.")
